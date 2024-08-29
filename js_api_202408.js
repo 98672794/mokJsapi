92474bb8c1ff244800b7d39c45f73543a05f739b
moksurky <95267354+98672794@users.noreply.github.com>
2024-08-30 02:07:28 +0800

    Update js_api_202408.js

--- js_api_202408.js
+++ js_api_202408.js
@@ -1,4 +1,4 @@
-console('印度线路板提炼黄金！合理又牛逼：');
+console.log('印度线路板提炼黄金！合理又牛逼：');
 
 
  $(document).ready(function() {    $('.ddddddd').text('© ' + new Date().getFullYear() + ' All rights reserved by ' + location.hostname).css({'font-size': 'medium'});    });
